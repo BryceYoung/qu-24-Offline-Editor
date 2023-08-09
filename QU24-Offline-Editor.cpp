1f115ec30815416b63be43a37a8e17212587cb93
BryceYoung <bryce.young18@icloud.com>
2023-08-09 20:17:02 +0930

    LoadShow, LoadScene, fileExists completed

--- QU24-Offline-Editor.cpp
+++ QU24-Offline-Editor.cpp
@@ -1,33 +1,37 @@
-
 #include "QU24-Offline-Editor.h"
-int main(){
-  std::istream* ifs = new std::ifstream("SCENE004.DAT",std::ios_base::binary);
-  kaitai::kstream* val = new kaitai::kstream(ifs);
-  qu_scene_t g = qu_scene_t(val);
-  for(int i = 0;i<24;i++){
-    (*g.inputs())[i]->SetMute(qu_scene_t::channel_entry_t::Muted);
-  }
-  kaitai::kstream* ret = g.Write("SCENE004.DAT");
-  ret->getFStream()->close();
-    qu_scene_t* scenes[72];
-    char string[] = "Scenes/SCENE004.DAT";
-    int index = 0;
-    for(int i = 0;i<10;i++){
-      for (int j = 0;j<10;j++){
-        if(i*10+j < 4) continue;
-        if(i*10+j > 71) continue;
-        string[13] = i + '0';
-        string[14] = j + '0';
-        //std::cout << string << std::endl;
-        std::istream* ifs = new std::ifstream(string,std::ios_base::binary);
-        kaitai::kstream* val = new kaitai::kstream(ifs);
-        scenes[index] = new qu_scene_t(val);
-        //(*scenes[index]->inputs())[0]->SetMute(qu_scene_t::channel_entry_t::UnMuted);
-        for(int k = 0;k<24;k++){
-          std::cout << (int)(*scenes[index]->inputs())[k]->GetMute() << " ";
-        }
-        std::cout << std::endl;
-        index++;
-      }
+#include <cstring>
+#include <unistd.h>
+
+bool fileExists(std::string filename){
+  return ( access( filename.c_str(), F_OK ) != -1 );
+}
+
+qu_scene_t* LoadScene(std::string filename){
+  if(!fileExists(filename))return NULL;
+  std::istream* file = new std::ifstream(filename,std::ios::binary);
+  kaitai::kstream* stream = new kaitai::kstream(file);
+  return new qu_scene_t(stream);
+}
+
+struct Show* LoadShow(std::string filepath){
+  struct Show* show = new struct Show;
+  show->readPath = filepath;
+  char filename[14] = "/SCENE000.DAT";
+  for(int i = 0;i<10;i++){
+    for(int j = 0;j<10;j++){
+      filename[7] = i + '0';
+      filename[8] = j + '0';
+      show->scenes[i*10+j] = LoadScene(filepath + filename);
     }
+  }
+  return show;
+}
+
+
+int main(){
+  struct Show* show = LoadShow("Scenes");
+  for(int i = 0;i<100;i++){
+    if(show->scenes[i]==NULL) continue;
+    std::cout << i << ": " << (int)show->scenes[i]->inputs()->at(0)->GetMute() << std::endl;
+  }
 }
--- QU24-Offline-Editor.h
+++ QU24-Offline-Editor.h
@@ -1,1 +1,17 @@
 #include "qu_scene.h"
+
+struct Show{
+  qu_scene_t* scenes[100];
+  std::string readPath;
+  std::string writePath;
+  std::string Showname;
+};
+
+Show show = {NULL};
+
+bool fileExists(std::string);
+
+// Load Scenes from folder
+struct Show* LoadShow(std::string);
+
+qu_scene_t* LoadScene(std::string);
